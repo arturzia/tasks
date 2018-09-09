7554b1ca0ceceabf599bf8a461704365f98e33f1
arturzia <arturzia@gmail.com>
2018-09-09 21:40:17 +0200

    Task_19_2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -10,8 +10,8 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-//public class TasksApplication extends SpringBootServletInitializer {
-	public class TasksApplication {
+public class TasksApplication extends SpringBootServletInitializer {
+	//public class TasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
