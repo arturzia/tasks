fc188a4b35fb666f19a41c67f870bf6901ae6524
arturzia <arturzia@gmail.com>
2018-09-25 14:30:53 +0200

    Task_24.3

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -9,8 +9,8 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SimpleEmailServiceTest {
@@ -23,7 +23,7 @@ public class SimpleEmailServiceTest {
     @Test
     public void shouldSendEmail() {
         //Given
-        Mail mail = new Mail("arturzia@gmail.com", "Test", "Test Message","");
+        Mail mail = new Mail("test@test.com", "Test", "Test Message","");
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
@@ -31,10 +31,11 @@ public class SimpleEmailServiceTest {
         mailMessage.setText(mail.getMessage());
 
 
+
         //When
         simpleEmailService.send(mail);
 
         //Then
-       verify(javaMailSender, times(1)).send(mailMessage);
+        verify(javaMailSender, times(1)).send(mailMessage);
     }
 }
